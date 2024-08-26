c7e8c4e6f05f463693920c7ceec771d79b066557
drleovasconcelos <lfreire1985@gmail.com>
2024-08-26 12:18:53 -0300

    13 commit

--- Fullstack - Aulas/Modulo-JavaScripts/Aula23/viacep/scripts.js
+++ Fullstack - Aulas/Modulo-JavaScripts/Aula23/viacep/scripts.js
@@ -5,17 +5,34 @@ const cidade = document.querySelector('#cidade')
 const estado = document.querySelector('#estado')
 const cep = document.querySelector('#cep')
 
+function preencherEndereco(endereco) {
+    bairro.value = endereco.bairro
+    cidade.value = endereco.localidade
+    estado.value = endereco.uf
+    end.value = endereco.logradouro
+}
+
+function isCepValid(cep) {
+    // /^[0-9]+$/.test(cep) => para testar se é string ou número 
+    return cep.length === 8 && /^[0-9]+$/.test(cep) 
+}
 
 function buscarEndereco() {
     const cep = document.querySelector('#cep').value
     // https://viacep.com.br/ws/01001000/json/
     const url = `https://viacep.com.br/ws/${cep}/json/`
-    fetch(url)
-        .then(response => {
-            return response.json()
-        })
-        .then(dados => console.log(dados))
-}
 
+    if (isCepValid(cep)) {
+        fetch(url)
+            .then(response => {
+                return response.json()
+            })
+            .then(dados => preencherEndereco(dados))
+    }
+    else {
+        end.value = 'Cep inválido!!!'
+    }
+    // alt + shift + f = ajeita identação
+}
 // focusout / blur - quando tura do input ele aparece o endereço nos outros campos
 cep.addEventListener('blur', buscarEndereco)
