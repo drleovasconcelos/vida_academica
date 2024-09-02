358c027fc7bf618901f8b7d20ffb574de9810953
drleovasconcelos <lfreire1985@gmail.com>
2024-09-02 11:39:45 -0300

    19 commit

--- Fullstack - Aulas/Modulo-JavaScripts/Aula24 - funcao assincrona/projetopokemon/scripts.js
+++ Fullstack - Aulas/Modulo-JavaScripts/Aula24 - funcao assincrona/projetopokemon/scripts.js
@@ -1,1 +1,32 @@
+const pokemonName = document.querySelector('.pokemon-name')
+const pokemonNumber = document.querySelector('.pokemon-number')
+const pokemonImage = document.querySelector('.pokemon-image')
+
+const form = document.querySelector('.form')
+const input = document.querySelector('.buscar')
+const btnNext = document.querySelector('.btn-next')
+const btnPrev = document.querySelector('.btn-prev')
+
+let pokemonAtual = 100
+
+// link da api // https://pokeapi.co/api/v2/pokemon/ditto - detalhe = usar crase onde está o link
+async function fetchPokemon(pokemon){
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}` 
+    const response = await fetch(url)
+    // console.log(response) - teste
+    if (response.status === 200) {
+        const data = await response.json()
+        // console.log(data) - teste
+        return data
+    }
+}
+// fetchPokemon(pokemonAtual) - teste
+
+async function renderPokemon(pokemon) {
+    pokemonName.innerText = 'Carregando...'
+    pokemonNumber.innerText = ''
+}
+
+renderPokemon(pokemonAtual)
+
 
