7ba6550bf3be232a4ea88c54e70613f0bfc58dd7
drleovasconcelos <lfreire1985@gmail.com>
2024-09-02 12:11:05 -0300

    19 commit

--- Fullstack - Aulas/Modulo-JavaScripts/Aula24 - funcao assincrona/projetopokemon/scripts.js
+++ Fullstack - Aulas/Modulo-JavaScripts/Aula24 - funcao assincrona/projetopokemon/scripts.js
@@ -7,7 +7,7 @@ const input = document.querySelector('.buscar')
 const btnNext = document.querySelector('.btn-next')
 const btnPrev = document.querySelector('.btn-prev')
 
-let pokemonAtual = 150
+let pokemonAtual = 1
 
 // await espera o dado (dar um tempo)
 // link da api // https://pokeapi.co/api/v2/pokemon/ditto - detalhe = usar crase onde está o link
@@ -33,9 +33,27 @@ async function renderPokemon(data) {
         pokemonNumber.innerText = pokemon.id
         // sprites está a imagem do pokemon na API -> sprites/versions/generation-v/black-white
         pokemonImage.src = pokemon['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
-        
+        input.value = ''
+        pokemonAtual = pokemon.id
+    }
+    else {
+        pokemonImage.style.display = 'none'
+        pokemonName.innerText = 'Não encontrado :c'
+        pokemonNumber.innerText = ''
     }
 }
+
+// submit = enter
+form.addEventListener('submit', (e) => {
+    // impede que o formulário recarrege - continua a página aberta
+    e.preventDefault()
+    renderPokemon(input.value.toLowerCase())
+})
+
+btnNext.addEventListener('click', () => {
+    pokemonAtual++
+    renderPokemon(pokemonAtual)
+})
 
 renderPokemon(pokemonAtual)
 
